ace89f47f0aa14cc90d5eaff2bf3863b20f349a1
vbirdchong <cc.chong.chen@gmail.com>
2015-11-30 22:36:43 +0800

    add another function to support mulit names input

--- func_name_store.py
+++ func_name_store.py
@@ -27,10 +27,32 @@ def name_store(data, full_name):
 			people.append(full_name)
 		else:
 			data[label][name] = [full_name]
-			
+
+##实现多个名字同时存储, 重点是 full_names前的星号, 参数前的星号将所有值都放置在同一个元组中收集起来			
+def name_storeV2(data, *full_names):
+        for full_name in full_names:
+                names = full_name.split()
+                
+                if len(names) == 2:
+                        names.insert(1, '')
+                        
+                labels = ('first', 'middle', 'last')
+                
+                for label, name in zip(labels, names):
+                        people = lookup(data, label, name)
+                        if people:
+                                people.append(full_name)
+                        else:
+                                data[label][name] = [full_name]
+
 			
 MyNames = {}
 init(MyNames)
 name_store(MyNames, 'Magnus Lie Hetland')
 print lookup(MyNames, 'middle', 'Lie')
+
+name_storeV2(MyNames, 'Luke Skywalker', 'Anakin Skywalker')
+print lookup(MyNames, 'last', 'Skywalker')
+
+
 
