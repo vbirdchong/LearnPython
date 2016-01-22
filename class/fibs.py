f71e4dcacc7c6f98ca95cd0a372f0773e42a8eae
vbirdchong <cc.chong.chen@gmail.com>
2016-01-22 13:18:10 +0800

    add fibs.py

--- class/fibs.py
+++ class/fibs.py
@@ -1,19 +1,49 @@
+#!/usr/bin/env python
+# conding=utf-8
+
 __metaclass__  = type
 
-class Fibs:
-    def __init__(self):
-        self.a = 0
-        self.b = 1
-    def next(self):
-        self.a, self.b = self.b, self.a+self.b
-        return self.a
-    def __iter__(self):
-        return self
+
+'''
+使用迭代的方法实现fibs
+'''
+
+##class Fibs:
+##    def __init__(self, maxValue):
+##        self.max = maxValue
+##        self.a = 0
+##        self.b = 1
+##
+##    def __iter__(self):
+##        return self
+##
+##    def next(self):
+##        fibs = self.a
+##        if fibs > self.max:
+##            raise StopIteration
+##        self.a, self.b = self.b, self.a + self.b
+##        return fibs
+##
+##
+##if __name__ == "__main__":
+##    fibs = Fibs(10000)
+##    print list(fibs)
 
 
-fibs = Fibs()
 
-for f in fibs:
-    if f > 1000:
-        print f
-        break
+'''
+使用generator的方法实现fibs
+'''
+
+def fibs(maxValue):
+    n = 0
+    a, b = 0, 1
+    while n < maxValue:
+        yield a
+        a, b = b, a + b
+        n += 1
+
+if __name__ == "__main__":
+    fib = fibs(10)
+    for i in fib:
+        print i
