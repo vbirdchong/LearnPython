f326b54162fabde108685cf33b0e73fb5531efb1
vbirdchong <cc.chong.chen@gmail.com>
2017-02-23 23:03:34 +0800

    add record rating info

--- douban/doubanSearch.py
+++ douban/doubanSearch.py
@@ -6,58 +6,82 @@
 
 import bs4
 import requests
+import re
+
 
 WEB_SITE = 'https://book.douban.com/'
-FIRST_PAGE_TAG = 'tag/%E7%BC%96%E7%A8%8B'
+FIRST_PAGE_TAG = 'tag/%E7%BC%96%E7%A8%8B?start=0&type=T'
+# FIRST_PAGE_TAG = 'tag/%E7%BC%96%E7%A8%8B?start=980&type=T'
 
-# 'https://book.douban.com/tag/%E7%BC%96%E7%A8%8B'
 
-
-def save_to_file(bookList):
+def save_to_file(bookList, ratingNumList):
     recordFile = open('bookList.txt', 'wb')
-    for book in bookList:
-        recordFile.write(book + '\r\n')
-
+    for i in range(len(bookList)):
+        recordFile.write(str(i+1) + '. ' + bookList[i] + ' :' + ratingNumList[i] + '\r\n')
     recordFile.close()
 
 
+def get_next_page_link(soup):
+    nextPageInfo = soup.select('.next a')
+    return nextPageInfo[0].get('href')
+
+
+def get_rating_num(subjectItemInfo):
+    ratingInfo = subjectItemInfo.select('[class="rating_nums"]')
+    if len(ratingInfo) != 0:
+        # this is a unicode type
+        return ratingInfo[0].get_text()
+    else:
+        return '0.0'
+
+
+def get_book_info(soup, bookList, ratingNumList,pageIndex):
+    print('Searching page:' + str(pageIndex / 20 + 1))
+    for subjectItem in soup.select('[class="subject-item"]'):
+        for itemInfo in subjectItem.select('a[title]'):
+            bookName = itemInfo.get('title')
+            try:
+                bookList.append(bookName.encode('gbk'))
+                ratingNum = get_rating_num(subjectItem)
+                ratingNumList.append(ratingNum.encode('utf-8'))
+            except UnicodeError:
+                print('''Can not decode the book name in page:''' + str(pageIndex / 20 + 1))
+
+
+def get_soup_from_link(pageLink):
+    res = requests.get(WEB_SITE + pageLink)
+    try:
+        res.raise_for_status()
+    except Exception as e:
+        raise('There is an error' + str(e))
+
+    return bs4.BeautifulSoup(res.text)
+
+
+def is_reached_the_last_page(soup):
+    if len(soup.select('div [class="paginator"]')) == 0:
+        return True
+    else:
+        return False
+
+
 def main():
-    page_tag = FIRST_PAGE_TAG
+    pageLink = FIRST_PAGE_TAG
+    bookList = []
+    ratingNumList = []
+    pageIndexRegex = re.compile(r'start=(\d+)', flags=0)
 
     while True:
-        res = requests.get(WEB_SITE + page_tag)
-        try:
-            res.raise_for_status()
-        except Exception as e:
-            print('There is an error ' + str(e))
+        soup = get_soup_from_link(pageLink)
+        if is_reached_the_last_page(soup):
+            print('''We reached the last page, got book number:%d''' % (len(bookList)))
+            break
 
-        htmlSoup = bs4.BeautifulSoup(res.text)
+        pageIndex = int(pageIndexRegex.search(pageLink).group(1))
+        get_book_info(soup, bookList, ratingNumList, pageIndex)
+        pageLink = get_next_page_link(soup)
 
-        bookList = []
-        # print(type(htmlSoup.select('a')))
-
-        for elemt in htmlSoup.select('a'):
-            if elemt.attrs.has_key('title'):
-                book = elemt.attrs.get('title')
-                try:
-                    print(book)
-                    # for chinese, we need encode the data to 'gb2312',
-                    # then we can save to file correctly
-                    bookList.append(book.encode('gbk'))
-                except UnicodeEncodeError as e:
-                    print('Can not decode the book name...')
-
-        # save_to_file(bookList)
-
-        # pageInfo = htmlSoup.select('div [class="paginator"]')
-        pageInfo = htmlSoup.select('.next a')
-
-        print(pageInfo)
-        print(pageInfo[0].attrs)
-        print(pageInfo[0].get('href'))
-        page_tag = pageInfo[0].get('href')
-
-        # need add break condition
+    save_to_file(bookList, ratingNumList)
 
 
 if __name__ == '__main__':
