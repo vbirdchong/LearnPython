1a566aca150e036bc028ee907b23da63991e4585
vbirdchong <cc.chong.chen@gmail.com>
2017-02-27 23:02:29 +0800

    add excel file

--- douban/doubanBookTag.py
+++ douban/doubanBookTag.py
@@ -4,9 +4,9 @@
 
 import requests
 import bs4
-import re
 import sys
 import urllib
+import openpyxl
 
 
 reload(sys)
@@ -16,26 +16,28 @@ sys.setdefaultencoding('utf8')
 def dou_ban_book_search(bookTagLists):
     bookLists = []
     for bookTag in bookTagLists:
-        print(bookTag.decode())
         bookList = book_search(bookTag)
+        # sorted the data by rating num
+        bookList = sorted(bookList, key=lambda x: x[1], reverse=True)
+        bookLists.append(bookList)
 
     return bookLists
 
 
 def book_search(bookTag):
     bookList = []
-    pageIdx = 49
+    # pageIdx = 49  # test page index
+    pageIdx = 0
 
     while True:
+        print('Getting book info for tag:%s on Page: %d' % (bookTag.decode(), pageIdx + 1))
         url = 'https://book.douban.com/tag/' + urllib.quote(bookTag) \
                 + '?start=' + str(pageIdx * 20) + '&type=T'
-        print(url)
 
         res = requests.get(url)
         try:
             res.raise_for_status()
             soup = bs4.BeautifulSoup(res.text)
-            # print(soup)
         except Exception as e:
             print('There was an error for get url...')
             raise e
@@ -43,56 +45,71 @@ def book_search(bookTag):
         subjectListSoup = soup.select('div[id="subject_list"]')
         subjectItem = subjectListSoup[0].select('li[class="subject-item"]')
         if len(subjectItem) == 0:
-            print('We reached the last page.')
+            print('We reached the last page, got %d books...' % (len(bookList)))
             break
         else:
             for bookSubjectItem in subjectItem:
                 bookList.append(get_book_info(bookSubjectItem))
 
         pageIdx += 1
-        print('Geting book info on Page: %d' % (pageIdx))
     return bookList
 
 
 def get_book_info(bookSubjectItem):
-    bookTitle = bookSubjectItem.select('a[title]')[0].get('title') # maybe we will use encode()
-    # print(bookTitle)
+    bookTitle = bookSubjectItem.select('a[title]')[0].get('title')  # maybe we will use encode()
     bookUrl = bookSubjectItem.select('a[href]')[0].get('href')
-    # print(bookUrl)
     authorInfo, pressInfo = get_book_base_info(bookSubjectItem)
 
     try:
-        ratingNum = bookSubjectItem.select('span[class="rating_nums"]')[0].get_text()
-    except Exception as e:
+        ratingNum = bookSubjectItem.select('span[class="rating_nums"]')[0].get_text().encode('utf-8')
+    except:
         ratingNum = '0.0'
-    # print(ratingNum)
 
-    print(bookTitle)
-    print(authorInfo)
-    print(pressInfo)
-    print(ratingNum)
-    return [bookTitle, authorInfo, pressInfo, ratingNum]
+    return [bookTitle, ratingNum, authorInfo, pressInfo, bookUrl]
 
 
 def get_book_base_info(bookSubjectItem):
     bookPublic = bookSubjectItem.select('div[class="pub"]')[0].get_text().strip()
     infoList = bookPublic.split('/')
     try:
-        authorInfo = 'Author/Translator: ' + '/'.join(infoList[0:-3])
+        authorInfo = 'Author: ' + '/'.join(infoList[0:-3])
     except:
-        authorInfo = 'Author/Translatro: ' + 'None'
+        authorInfo = 'Author: ' + 'None'
 
     try:
-        pressInfo = 'Press: ' + '/'.join(infoList[-3:])
+        pressInfo = '/'.join(infoList[-3:])
     except:
-        pressInfo = 'Press:' + 'None'
+        pressInfo = 'None'
 
     return authorInfo, pressInfo
 
 
+def save_book_to_excel(bookLists, bookTagLists):
+    wb = openpyxl.Workbook()
+    ws = []
+    for tag in range(len(bookTagLists)):
+        ws.append(wb.create_sheet(title=bookTagLists[tag].decode()))
+
+    for tag in range(len(bookTagLists)):
+        ws[tag].append(['Id', 'Titil', 'Rating', 'Author', 'Press', 'Link'])
+        count = 1
+        for bookList in bookLists[tag]:
+            ws[tag].append([count, bookList[0], float(bookList[1]), bookList[2], bookList[3], bookList[4]])
+            count += 1
+
+    fileName = 'bookList'
+    for i in range(len(bookTagLists)):
+        fileName += ('-' + bookTagLists[i].decode())
+    fileName += '.xlsx'
+    wb.save(fileName)
+    print('Save to file:%s successfully...' % (fileName))
+
+
 def main():
     bookTagLists = ['编程', '科学']
-    bookList = dou_ban_book_search(bookTagLists)
+    # bookTagLists = ['编程', '文学', '小说']
+    bookLists = dou_ban_book_search(bookTagLists)
+    save_book_to_excel(bookLists, bookTagLists)
 
 
 if __name__ == '__main__':
