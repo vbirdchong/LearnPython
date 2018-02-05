476f73ffc0b762e516345f07c68f2971a60ffca0
vbirdchong <cc.chong.chen@gmail.com>
2018-02-05 13:47:06 +0800

    skip some filders and write the log to reprot

--- PythonScript/top_file_size.py
+++ PythonScript/top_file_size.py
@@ -3,6 +3,7 @@
 import os
 import sys
 import time
+import ctypes
 
 USAGE_INFO = '''
 Usage: python top_file_size.py path top_number
@@ -11,9 +12,31 @@ Usage: python top_file_size.py path top_number
 
 ONE_MB = 1048576
 ONE_GB = 1073741824
+SKIP_FOLDER = ['System Volume Information', 'Office365_Update']
+
+def run_as_admin(argv=None, debug=False):
+    shell32 = ctypes.windll.shell32
+    if argv is None and shell32.IsUserAnAdmin():
+        return True
+
+    if argv is None:
+        argv = sys.argv
+    if hasattr(sys, '_MEIPASS'):
+        # Support pyinstaller wrapped program.
+        arguments = map(unicode, argv[1:])
+    else:
+        arguments = map(unicode, argv)
+    argument_line = u' '.join(arguments)
+    executable = unicode(sys.executable)
+    if debug:
+        print 'Command line: ', executable, argument_line
+    ret = shell32.ShellExecuteW(None, u"runas", executable, argument_line, None, 1)
+    if int(ret) <= 32:
+        return False
+    return None
 
 def get_folder_size(folder):
-    print('Checking ' + folder + '...')
+    print('Checking ' + folder + ' ...')
     total_size = os.path.getsize(folder)
     try:
         for item in os.listdir(folder):
@@ -27,13 +50,22 @@ def get_folder_size(folder):
 
     return total_size
 
+def is_need_skip(folder_name):
+    ret = False
+    if (folder_name.startswith('.')) or (folder_name in SKIP_FOLDER):
+        ret = True
+    return ret
+
 def get_top_size_folder_info(directory, top_number):
     folder_info = {}
     for root, subdir, files in os.walk(directory):
         for folder in subdir:
-            path = os.path.join(root, folder)
-            size = get_folder_size(path)
-            folder_info[path] = size
+            if is_need_skip(folder):
+                continue
+            else:
+                path = os.path.join(root, folder)
+                size = get_folder_size(path)
+                folder_info[path] = size
 
     size_sorted = sorted(zip(folder_info.values(), folder_info.keys()), reverse=True)
     top_size_folder_info = []
@@ -43,17 +75,26 @@ def get_top_size_folder_info(directory, top_number):
         print_info = ''
         if folder_size > ONE_GB:
             print_info = '%10.2f GB' % (folder_size / ONE_GB) + '\t'
-        else:
+        elif folder_size > ONE_MB:
             print_info = '%10.2f MB' % (folder_size / ONE_MB) + '\t'
+        else:
+            print_info = '%10.2f KB' % folder_size + '\t'
 
         print_info += folder_path
         top_size_folder_info.append(print_info)
     return top_size_folder_info
 
+def write_to_report_file(path, info):
+    os.chdir(path)
+    with open('Top_file_size_report.log', 'wt') as f:
+        for data in info:
+            f.write(data + '\n')
+
 def main():
     if len(sys.argv) == 3:
         start_time = time.clock()
         info = get_top_size_folder_info(sys.argv[1], int(sys.argv[2]))
+        write_to_report_file(sys.argv[1], info)
         end_time = time.clock()
         for data in info:
             print(data)
